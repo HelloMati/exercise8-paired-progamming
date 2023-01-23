40793585ea8f60049d9d06a3bae4f37bcfc93b29
Ellen Houghton <ellen.a.ash@gmail.com>
2023-01-23 20:39:46 +0000

    add notes

--- anatomy.py
+++ anatomy.py
@@ -1,11 +1,16 @@
+# import a package/ library
 import sys
 
+# declare a variable that is the number of items in the list called argv
 arguments = len(sys.argv)
 
+# conditional statement, if the variable called arguments contains more than 1 item run the built-in function to print the string
 if arguments > 1:
     print("Too many arguments")
 else:
+    # if the argument count is greater than 1 then print the string 'hello' with the variable called location
     location = 'World'
     print("Hello", location)
 
+# run the print function to print the string plus the first argument in the list which is located using the position index at 0
 print('Goodbye from ' + sys.argv[0])
--- say_my_name.py
+++ say_my_name.py
@@ -1,3 +1,5 @@
+# declare 2 variables that contain strings
 first_name = "Ellen"
 last_name = "Houghton"
+# use built-in print function to print the variables with a space
 print(first_name + " " + last_name)
