89293f0fb38ffe50beba598fd3044eb69d9ae969
Ellen Houghton <ellen.a.ash@gmail.com>
2023-01-24 09:33:11 +0000

    added additional variables

--- say_my_name.py
+++ say_my_name.py
@@ -8,3 +8,9 @@ first_name = "Victor"
 last_name = "Meldrew"
 # use built-in print function to print the variables with a space
 print(first_name + " " + last_name)
+
+# adding another name because this is fun and I have never collaborated in code before
+greeting = "Hello"
+person = "Mati"
+
+print(greeting + " " + person)
