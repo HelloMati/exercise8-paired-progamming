76c2fa5b4ce4f46f0a2e00406a75d7fff4e724f8
Ellen Houghton <ellen.a.ash@gmail.com>
2023-01-24 09:45:03 +0000

    looking into the .idea file that appeared

--- say_my_name.py
+++ say_my_name.py
@@ -14,3 +14,10 @@ greeting = "Hello"
 person = "Mati"
 
 print(greeting + " " + person)
+
+# the .idea file appeared on my computer after I pulled the file from github
+# i looked it up and found it has been an issue for others https://github.com/toptal/gitignore.io/issues/360
+# stack overflow suggested it was ok to pop it into gitignore
+# and i found this link about the security risk for the .idea file
+# i don't understand everything here but I am dropping the link here for future reference
+# https://www.acunetix.com/vulnerabilities/web/jetbrains-idea-project-directory/
